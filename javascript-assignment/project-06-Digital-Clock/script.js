590e4e16ffcd02207e942131edc65a4d52b7f2df
Atikur Rahman <atikurrahman160313@gmail.com>
2026-06-27 21:50:58 +0600

    Add Digital Clock functionality with HTML, CSS, and JavaScript

--- javascript-assignment/project-06-Digital-Clock/script.js
+++ javascript-assignment/project-06-Digital-Clock/script.js
@@ -1,9 +1,18 @@
-let name = "Atikur Rahman";
-let age = 24;
-let district = "Narayanganj";
-let isStudent = true;
-
-console.log("Name:", name);
-console.log("Age:", age);
-console.log("District:", district);
-console.log("Student:", isStudent);
+const timeElement = document.querySelector("#time");
+function updateClock() {
+    const now = new Date();
+    let hours = now.getHours();
+    let minutes = now.getMinutes();
+    let seconds = now.getSeconds();
+    let period = "AM";
+    if (hours >= 12) {
+        period = "PM";
+    }
+    hours = hours % 12 || 12;
+    hours = String(hours).padStart(2, "0");
+    minutes = String(minutes).padStart(2, "0");
+    seconds = String(seconds).padStart(2, "0");
+    timeElement.textContent = `${hours}:${minutes}:${seconds} ${period}`;
+}
+updateClock();
+setInterval(updateClock, 1000);
